794cc759e01424e3a7b5faac6ba3bc12fb989e9b
sudip sarkar <sudipskr321@gmail.com>
2025-04-21 12:59:28 +0530

    v-1.0.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Experience from "../components/Experience.jsx";
 
 const Home = () => {
   return (
-    <div className={"min-h-screen bg-black p-8 sm:p-14 font-[Inter]"}>
+    <div className={"min-h-screen bg-black p-5 sm:p-14 font-[Inter]"}>
       <main className={"max-w-[700px] mx-auto w-full"}>
         <Hero />
         <Technologies />
